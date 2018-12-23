79a5793437efe8a0ad10599af9300ff115d6fa34
Andre Gonzaga <degutos.btu@gmail.com>
2018-12-23 11:58:02 +0000

    Adding booleans basic

---
+++ booleans.py
@@ -0,0 +1,17 @@
+# Booleans is True or False
+# True or False should be with first capital letter
+
+# Examples
+1 > 2
+False
+
+1 == 1
+True
+
+
+# How to keep a variable without value
+b = None
+
+print(b)
+print(type(b))
+
